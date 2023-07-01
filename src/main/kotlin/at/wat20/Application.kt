23040c0932d0f959352edf5944da7ecda40508b6
Marcel22R <marieder22@gmail.com>
2023-07-01 16:23:24 +0200

    Initial commit.

---
+++ src/main/kotlin/at/wat20/Application.kt
@@ -0,0 +1,22 @@
+package at.wat20
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import at.wat20.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureTemplating()
+    configureDatabases()
+    configureSockets()
+    configureMonitoring()
+    configureRouting()
+}
